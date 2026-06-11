284141a782a5460b9680cde51a41969b19f32951
Eric Leal <eric.gsleal13@gmail.com>
2026-06-11 16:30:40 -0400

    Jogo de apostas prontos

--- aposta.cpp
+++ aposta.cpp
@@ -7,48 +7,79 @@ const float russiaXmarrocos = 1.1;
 const float francaXargentina = 1.2;
 const float germainiaXcostademarfim = 4.2;
 
-int ptuser1 = 0; // ptuser2, ptuser3;
+float timeVez = 0.0;
+int jogo = 1; 
+
+int pontos = 0; 
+int ptuser1, ptuser2, ptuser3;
 float aposta1 = 0; // aposta2, aposta3;
 
 int calculo (float aposta1) {
 
-        int golsCasa = (int)brasilXegito;             // 3
-        int golsVisitante = (int)(brasilXegito * 10) % 10; // 0
+        int golsCasa = (int)timeVez;             // 3
+        int golsVisitante = (int)(timeVez * 10) % 10; // 0
 
         int c1 = (int)aposta1;             // Gols do Brasil na aposta 1
-        int v1 = (int)(aposta1 - c1) * 10; // Gols do Egito na aposta 1
+        int v1 = (int)(aposta1 * 10) % 10; // Gols do Egito na aposta 1
 
-        if (aposta1 == brasilXegito){
-            ptuser1 = ptuser1 + 10;
+        if (aposta1 == timeVez){
+            pontos = pontos + 10;
         } else if ((golsCasa > golsVisitante) && (c1 > v1)){
-            ptuser1 = ptuser1 + 5;
-        } else if ((golsCasa > golsVisitante) && (c1 > v1)){
-            ptuser1 = ptuser1 + 5;
+            pontos = pontos + 5;
+        } else if ((golsCasa < golsVisitante) && (c1 < v1)){
+            pontos = pontos + 5;
         }
 
-        return ptuser1;
+        return pontos;
+}
+
+float jogos (int jogo) {
+
+        if (jogo == 1) {
+            timeVez = brasilXegito;
+        } else if (jogo == 2) {
+            timeVez = russiaXmarrocos;
+        } else if (jogo == 3) {
+            timeVez = francaXargentina;
+        } else if (jogo == 4) {
+            timeVez = germainiaXcostademarfim;
+        }   
+    return timeVez;
 }
 
 
 int main() {
-    bool sessao = true;
 
-    while (sessao) {
+    cout << "=== BEM-VINDO AO SISTEMA DE APOSTAS ===" << endl;
 
-        cout << "=== BEM-VINDO AO SISTEMA DE APOSTAS ===" << endl;
-        cout << "As apostas sao em numeros reais. Ex: 1.0 = 1 x 0, 1.1 = 1 x 1\n" << endl;
+    for (int jogador = 1; jogador <= 3; jogador++) {
         
-        // --- LEITURA DAS APOSTAS ---
-        cout << "Apostador 1 - Digite seu palpite para Brasil x Egito: ";
-        cin >> aposta1;
-        calculo(aposta1);
+        pontos = 0; 
+        for (jogo = 1; jogo <= 4; jogo++) {
+            cout << "1º Brasil vs Egito\n";
+            cout << "2º Rússia vs Marrocos\n";
+            cout << "3º França vs Argentina\n";
+            cout << "4º Alemanha vs Costa do Marfim\n";
+            cout << "As apostas são em numeros reais. Ex: 1.0 = 1 x 0, 1.1 = 1 x 1\n" << endl;
 
-        cout << "Apostador 1 - Digite seu palpite para Russia x Marrocos: ";
-        cin >> aposta1;
-        calculo(aposta1);
-
-        cout << ptuser1 << endl;
-        sessao = false;
-        
+            // --- LEITURA DAS APOSTAS ---
+            cout << "Apostador " << jogador << " - Digite seu palpite para o " << jogo << "º jogo:\n";
+            cin >> aposta1;
+            jogos(jogo);
+            calculo(aposta1);
+        }
+        if (jogador == 1) {
+            ptuser1 = pontos;
+        } else if (jogador == 2) {
+            ptuser2 = pontos;
+        } else if (jogador == 3) {
+            ptuser3 = pontos;
+        }
+        cout << "Apostas do Jogador " << jogador << " Registradas!!\n" << endl;
     }
+    
+    cout << "=== Placar de acertos: === \n" << endl;
+    cout << "Jogador 1: " << ptuser1 << " pontos\n" << endl;
+    cout << "Jogador 2: " << ptuser2 << " pontos\n" << endl;
+    cout << "Jogador 3: " << ptuser3 << " pontos\n" << endl;
 }
